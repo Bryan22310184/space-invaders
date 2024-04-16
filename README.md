6c9f446fa40923a839a81fe632788ffe98946dbb
Bryan22310184 <159966204+Bryan22310184@users.noreply.github.com>
2024-04-16 23:59:02 +0000

    Version 2

--- README.md
+++ README.md
@@ -15,4 +15,4 @@ El objetivo de este proyecto es hacer un juego ascii art basado en el famoso jue
 - src/ - Contiene el codigo fuente
 - include/ - Los archivos de cabecera 
 - assets/ - Contiene los recursos del proyecto
-- docs/ - 
+- docs/ - Contiene la documentacion del proyecto
---
+++ src/tui_test.cpp
@@ -0,0 +1,23 @@
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/screen/string.hpp>
+
+using namespace ftxui;
+
+int main() {
+    
+    //crea los elementos de la interfaz de usuario 
+    Element label = text ("Hola, mundo!");
+    Element container = hbox({label});
+
+//crea la pantalla y establece el contenido
+auto screen = Screen::Create(
+    Dimension:: Full(),
+    Dimension:: Fit (container)
+
+);
+Render (screen, container);
+screen.print();
+
+return 0;
+}
